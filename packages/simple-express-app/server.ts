87f9441e32ee135b2808ebbb6216804d58534aab
chauminhquang <quangcm@unicloud.com.vn>
2023-03-07 17:01:56 +0700

    feat: adding submodule in git

--- packages/simple-express-app/server.ts
+++ packages/simple-express-app/server.ts
@@ -1,16 +1,26 @@
 import express from "express";
 import cors from "cors";
 
+import _ from "lodash";
+
+import { QueryPayload } from "simple-shared-data";
+
 const app = express();
 
 app.use(cors());
 
+app.use((_req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.json({
-    foo: "bar",
-  });
+  const responseData: QueryPayload = {
+    payload: _.snakeCase("Server data returned successfully"),
+  };
+  res.json(responseData);
 });
 
 app.listen(port, () => {
--- packages/simple-react-app/src/App.tsx
+++ packages/simple-react-app/src/App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
+import _ from "lodash";
 import "./App.css";
+import { QueryPayload } from "simple-shared-data";
+
+import DarkMode from "./react-dark-mode/src/DarkMode";
 
 function App() {
   return (
@@ -10,19 +14,20 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <DarkMode />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {_.toUpper("Learn React")}
         </a>
         <button
           onClick={() => {
             fetch("http://localhost:5000/", {})
               .then((res) => res.json())
-              .then((data) => console.log(data));
+              .then((data: QueryPayload) => console.log(data.payload));
           }}
         >
           Get some data
